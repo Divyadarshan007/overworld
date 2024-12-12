03a2917b4df4573bedb6856eeab0d9d81fbe1673
divyadarshan007 <dasdivyadarshan@gmail.com>
2024-12-12 17:08:56 +0530

    overworld website

---
+++ assets/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "2",
+  centeredSlides: true,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
