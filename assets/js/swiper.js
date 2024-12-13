cc3d03b73b34d8a92593b64eec2545131f40af53
divyadarshan007 <dasdivyadarshan@gmail.com>
2024-12-13 17:12:05 +0530

    added merchandise section, footer and media query

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,8 +2,36 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: "2",
   centeredSlides: true,
   spaceBetween: 0,
+  loop: true,
   autoplay: {
     delay: 1500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+});
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: "5",
+  centeredSlides: true,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 3500,
     disableOnInteraction: false,
   },
   loop: true,
